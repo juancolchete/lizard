924768b0c9869d841b54ac8c9fc91ea66098b48c
juancolchete <juanudkp@gmail.com>
2023-06-16 22:06:52 -0300

    feat: improve basic command

--- lua/lizard/init.lua
+++ lua/lizard/init.lua
@@ -1,1 +1,15 @@
-print("Hello World")
+local M = {}
+
+function M.setup()
+    -- Define the command
+    vim.cmd([[command! -nargs=* Lizard lua require('myplugin').execute_lizard(<f-args>)]])
+
+    -- Define the function to be executed
+    function M.execute_lizard(args)
+        print("Executing Lizard command with arguments:", args)
+        -- Your custom logic here
+    end
+end
+
+return M
+
