9a898ed6cc68e9c9b59262f19f539ae0e9097717
juancolchete <juanudkp@gmail.com>
2023-06-17 20:51:35 -0300

    chore: change resolution

--- lua/lizard/init.lua
+++ lua/lizard/init.lua
@@ -4,7 +4,7 @@ function Lizard.open(...)
     buffer1 = vim.api.nvim_create_buf(false, true)
     window1 = vim.api.nvim_open_win(buffer1, true, {
         style="minimal", relative='editor',
-        row=3, col=3, width=30, height=9, border='single'
+        row=3, col=3, width=640, height=480, border='single'
     })
 end
 
