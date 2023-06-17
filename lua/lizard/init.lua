32b46f1e74a17f36a8cb57831f5616d6b6b47ec2
juancolchete <juanudkp@gmail.com>
2023-06-17 19:30:47 -0300

    debug: add dependencies to test view

--- lua/lizard/init.lua
+++ lua/lizard/init.lua
@@ -1,10 +1,44 @@
+local View = require("trouble.view")
+local config = require("trouble.config")
+local colors = require("trouble.colors")
+local util = require("trouble.util")
+
 local Lizard = {}
 
 local view
 
-if view and action == "on_win_enter" then
-    print("Hello World")
+local function get_opts(...)
+  local args = { ... }
+  if vim.tbl_islist(args) and #args == 1 and type(args[1]) == "table" then
+    args = args[1]
+  end
+  local opts = {}
+  for key, value in pairs(args) do
+    if type(key) == "number" then
+      local k, v = value:match("^(.*)=(.*)$")
+      if k then
+        opts[k] = v
+      elseif opts.mode then
+        util.error("unknown option " .. value)
+      else
+        opts.mode = value
+      end
+    else
+      opts[key] = value
+    end
+  end
+  opts = opts or {}
+  util.fix_mode(opts)
+  config.options.cmd_options = opts
+  return opts
 end
+
+
+function Lizard.open(...)
+  local opts = get_opts(...)
+  view = View.create(opts)
+end
+
 
 return Lizard
 
--- plugin/lizard.vim
+++ plugin/lizard.vim
@@ -1,5 +1,5 @@
 command! -nargs=* -complete=custom,s:complete Lizard lua require'lizard'.open(<f-args>)
 command! -nargs=* -complete=custom,s:complete LizardToggle lua require'lizard'.toggle(<f-args>)
-command! TroubleClose lua require'lizard'.close()
-command! TroubleRefresh lua require'lizard'.refresh()
+command! LizardClose lua require'lizard'.close()
+command! LizardRefresh lua require'lizard'.refresh()
 
