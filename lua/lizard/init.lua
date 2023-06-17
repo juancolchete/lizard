9bde9d896a341b437d459dfeab9a5b3916cfb36e
juancolchete <juanudkp@gmail.com>
2023-06-17 20:29:23 -0300

    refactor: remove unused code

--- lua/lizard/init.lua
+++ lua/lizard/init.lua
@@ -1,37 +1,4 @@
-local View = require("trouble.view")
-local config = require("trouble.config")
-local colors = require("trouble.colors")
-local util = require("trouble.util")
-
 local Lizard = {}
-
-local view
-
-local function get_opts(...)
-  local args = { ... }
-  if vim.tbl_islist(args) and #args == 1 and type(args[1]) == "table" then
-    args = args[1]
-  end
-  local opts = {}
-  for key, value in pairs(args) do
-    if type(key) == "number" then
-      local k, v = value:match("^(.*)=(.*)$")
-      if k then
-        opts[k] = v
-      elseif opts.mode then
-        util.error("unknown option " .. value)
-      else
-        opts.mode = value
-      end
-    else
-      opts[key] = value
-    end
-  end
-  opts = opts or {}
-  util.fix_mode(opts)
-  config.options.cmd_options = opts
-  return opts
-end
 
 local function create_win()
   -- We save handle to window from which we open the navigation
