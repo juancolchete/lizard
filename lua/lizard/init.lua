30079cb8e30e6e55a43966211fc5027b71eac626
juancolchete <juanudkp@gmail.com>
2023-06-17 23:11:12 -0300

    debug: test via lua file instead of txt

--- lua/lizard/init.lua
+++ lua/lizard/init.lua
@@ -1,20 +1,15 @@
+local draw = require("lizard.draw")
 local Lizard = {}
 
 function Lizard.open(...)
   local buffer1 = vim.api.nvim_create_buf(false, true)
-  print(os.getenv("PWD").."/lizard.txt")
-  local f = io.open(os.getenv("PWD").."/lizard.txt","r")
-  local lines = f:read("*all")
-  f:close()
-  for line in lines do
+  for line in draw do
     vim.api.nvim_buf_set_lines(buffer1, 0, -1, true, {line})
   end
   vim.api.nvim_open_win(buffer1, true, {
     style="minimal", relative='editor',
     row=3, col=50, width=200, height=50, border='single'
   })
-
-
 end
 
 
