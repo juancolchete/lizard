958c06b0ef9933320e08e0ffc4e813fbb8e0f56a
juancolchete <juanudkp@gmail.com>
2023-06-17 21:49:28 -0300

    fix: not appearing lizard on right place

--- lua/lizard/init.lua
+++ lua/lizard/init.lua
@@ -1,11 +1,7 @@
 local Lizard = {}
 
 function Lizard.open(...)
-    buffer1 = vim.api.nvim_create_buf(false, true)
-    window1 = vim.api.nvim_open_win(buffer1, true, {
-        style="minimal", relative='editor',
-        row=3, col=50, width=200, height=50, border='single'
-    })
+  buffer1 = vim.api.nvim_create_buf(false, true)
   vim.api.nvim_buf_set_lines(buf, 0, -1, true, [[
                 ____...---...___
 ___.....---"""        .       ""--..____
@@ -21,7 +17,12 @@ ___.....---"""        .       ""--..____
            \ \`-.__.-'/ /\|\|           .
           .'  `.    .'  `.
           |/\/\|    |/\/\|
-]])
+]])  
+  window1 = vim.api.nvim_open_win(buffer1, true, {
+    style="minimal", relative='editor',
+    row=3, col=50, width=200, height=50, border='single'
+  })
+  
  
 end
 
