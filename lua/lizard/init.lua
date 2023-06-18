2b48c2fbcc3084230c782879eacdf7678f630111
juancolchete <juanudkp@gmail.com>
2023-06-17 23:28:25 -0300

    debug: pass draw dynamicaly

--- lua/lizard/init.lua
+++ lua/lizard/init.lua
@@ -16,14 +16,10 @@ ___.....---"""        .       ""--..____
           .'  `.    .'  `.
           |/\/\|    |/\/\|
 ]]
-
 function Lizard.open(...)
   print(draw)
   local buffer1 = vim.api.nvim_create_buf(false, true)
-  for line in draw do
-    print(line)
-    vim.api.nvim_buf_set_lines(buffer1, 0, -1, true, {line})
-  end
+  vim.api.nvim_buf_set_lines(buffer1, 0, -1, true, {draw})
   vim.api.nvim_open_win(buffer1, true, {
     style="minimal", relative='editor',
     row=3, col=50, width=200, height=50, border='single'
