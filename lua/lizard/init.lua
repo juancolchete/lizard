298c16181232ec459d2cf02c05f39b467d02a54c
juancolchete <juanudkp@gmail.com>
2023-06-17 21:04:57 -0300

    debug new window size

--- lua/lizard/init.lua
+++ lua/lizard/init.lua
@@ -4,7 +4,7 @@ function Lizard.open(...)
     buffer1 = vim.api.nvim_create_buf(false, true)
     window1 = vim.api.nvim_open_win(buffer1, true, {
         style="minimal", relative='editor',
-        row=3, col=3, width=200, height=100, border='single'
+        row=3, col=3, width=100, height=50, border='single'
     })
 end
 
