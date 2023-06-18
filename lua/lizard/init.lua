75c001d1b176df8902b3a5ca5ad56047cd6eefe6
juancolchete <juanudkp@gmail.com>
2023-06-17 22:23:50 -0300

    feat: read file instead getting lizard from code

--- lua/lizard/init.lua
+++ lua/lizard/init.lua
@@ -1,14 +1,18 @@
 local Lizard = {}
 
 function Lizard.open(...)
-  buffer1 = vim.api.nvim_create_buf(false, true)
-  vim.api.nvim_buf_set_lines(buffer1, 0, -1, true, {"abc", "def"})  
-  window1 = vim.api.nvim_open_win(buffer1, true, {
+  local buffer1 = vim.api.nvim_create_buf(false, true)
+  local file = io.open("../../assets/lizard.txt")
+  local lines = file:lines()
+  for line in lines do
+    vim.api.nvim_buf_set_lines(buffer1, 0, -1, true, {line})
+  end
+  vim.api.nvim_open_win(buffer1, true, {
     style="minimal", relative='editor',
     row=3, col=50, width=200, height=50, border='single'
   })
-  
- 
+
+
 end
 
 
