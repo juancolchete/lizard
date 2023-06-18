6eaca0c7fafb3db710ae082f7518faf2274369b7
juancolchete <juanudkp@gmail.com>
2023-06-17 23:43:32 -0300

    debug: add lines

--- lua/lizard/init.lua
+++ lua/lizard/init.lua
@@ -16,11 +16,12 @@ ___.....---"""        .       ""--..____
           .'  `.    .'  `.
           |/\/\|    |/\/\|
 ]]
-draw = string.gsub(draw, "\n", "")
 function Lizard.open(...)
   print(draw)
   local buffer1 = vim.api.nvim_create_buf(false, true)
-  vim.api.nvim_buf_set_lines(buffer1, 0, -1, true, {draw})
+  for line in string.gmatch(draw, "\n") do
+    vim.api.nvim_buf_set_lines(buffer1, 0, -1, true, {line})
+  end
   vim.api.nvim_open_win(buffer1, true, {
     style="minimal", relative='editor',
     row=3, col=50, width=200, height=50, border='single'
--- plugin/lizard.vim
+++ plugin/lizard.vim
@@ -1,5 +1,1 @@
 command! -nargs=* -complete=custom,s:complete Lizard lua require'lizard'.open(<f-args>)
-command! -nargs=* -complete=custom,s:complete LizardToggle lua require'lizard'.toggle(<f-args>)
-command! LizardClose lua require'lizard'.close()
-command! LizardRefresh lua require'lizard'.refresh()
-
