edb5ea26778c4bdbb35eb5fd6216bfd7043a7bbb
juancolchete <juanudkp@gmail.com>
2023-06-18 00:50:28 -0300

    fix: change order

--- lua/lizard/init.lua
+++ lua/lizard/init.lua
@@ -18,14 +18,14 @@ ___.....---"""        .       ""--..____
 ]]
 
 function Lizard.open(...)
-  local buffer1 = vim.api.nvim_create_buf(false, true)
-  for str in string.gmatch(drawRaw, "([^\n]+)") do
-   vim.api.nvim_buf_set_lines(buffer1, -1, -1, true, {str})
-  end
+  local buffer1 = vim.api.nvim_create_buf(false, true) 
   vim.api.nvim_open_win(buffer1, true, {
     style="minimal", relative='editor',
     row=3, col=50, width=200, height=50, border='single'
   })
+  for str in string.gmatch(drawRaw, "([^\n]+)") do
+   vim.api.nvim_buf_set_lines(buffer1, -1, -1, true, {str})
+  end
 end
 
 
