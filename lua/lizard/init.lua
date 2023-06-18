7723b2b4ac6d845510c6ed13cbea8d10f3cbd910
juancolchete <juanudkp@gmail.com>
2023-06-18 00:53:46 -0300

    refactor: resize the window

--- lua/lizard/init.lua
+++ lua/lizard/init.lua
@@ -21,7 +21,7 @@ function Lizard.open(...)
   local buffer1 = vim.api.nvim_create_buf(false, true) 
   vim.api.nvim_open_win(buffer1, true, {
     style="minimal", relative='editor',
-    row=3, col=50, width=200, height=50, border='single'
+    row=3, col=50, width=40, height=16, border='single'
   })
   for str in string.gmatch(drawRaw, "([^\n]+)") do
    vim.api.nvim_buf_set_lines(buffer1, -1, -1, true, {str})
