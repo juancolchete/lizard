b6d6af05fbd3c78d7b14b2296e362d3a12e68f92
juancolchete <juanudkp@gmail.com>
2023-06-17 21:59:01 -0300

    fix: wrong buffer spell

--- lua/lizard/init.lua
+++ lua/lizard/init.lua
@@ -2,7 +2,7 @@ local Lizard = {}
 
 function Lizard.open(...)
   buffer1 = vim.api.nvim_create_buf(false, true)
-  vim.api.nvim_buf_set_lines(buf, 0, -1, true, {"abc", "def"})  
+  vim.api.nvim_buf_set_lines(buffer1, 0, -1, true, {"abc", "def"})  
   window1 = vim.api.nvim_open_win(buffer1, true, {
     style="minimal", relative='editor',
     row=3, col=50, width=200, height=50, border='single'
