26f018fb270b7d8e443da268977574679d8dda8e
juancolchete <juanudkp@gmail.com>
2023-06-17 21:34:58 -0300

    debug: test with buffer2

--- lua/lizard/init.lua
+++ lua/lizard/init.lua
@@ -2,11 +2,7 @@ local Lizard = {}
 
 function Lizard.open(...)
     buffer1 = vim.api.nvim_create_buf(false, true)
-    window1 = vim.api.nvim_open_win(buffer1, true, {
-        style="minimal", relative='editor',
-        row=3, col=50, width=50, height=25, border='single'
-    })
-  vim.api.nvim_out_write([[
+    buffer2 = [[
                 ____...---...___
 ___.....---"""        .       ""--..____
      .                  .            .
@@ -21,7 +17,12 @@ ___.....---"""        .       ""--..____
            \ \`-.__.-'/ /\|\|           .
           .'  `.    .'  `.
           |/\/\|    |/\/\|
-  ]])
+  ]]
+    window1 = vim.api.nvim_open_win(buffer2, true, {
+        style="minimal", relative='editor',
+        row=3, col=50, width=50, height=25, border='single'
+    })
+   
 end
 
 
