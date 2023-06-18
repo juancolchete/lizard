2b9d46612c696a6248563e75712fcc11afd11e30
juancolchete <juanudkp@gmail.com>
2023-06-17 22:04:01 -0300

    chore: redo buffer test

--- lua/lizard/init.lua
+++ lua/lizard/init.lua
@@ -1,25 +1,11 @@
 local Lizard = {}
 
 function Lizard.open(...)
-  local buffer1 = vim.api.nvim_create_buf(false, true)
-  vim.api.nvim_buf_set_lines(buffer1, 0, -1, true, {[[              ____...---...___
-___.....---"""        .       ""--..____
-     .                  .            .
- .             _.--._       /|
-        .    .'()..()`.    / /
-            ( `-.__.-' )  ( (    .
-   .         \        /    \ \
-       .      \      /      ) )        .
-            .' -.__.- `.-.-'_.'
- .        .'  /-____-\  `.-'       .
-          \  /-.____.-\  /-.
-           \ \`-.__.-'/ /\|\|           .
-          .'  `.    .'  `.
-          |/\/\|    |/\/\|
-jro]]})  
-  local window1 = vim.api.nvim_open_win(buffer1, true, {
+  buffer1 = vim.api.nvim_create_buf(false, true)
+  vim.api.nvim_buf_set_lines(buffer1, 0, -1, true, {"abc", "def"})  
+  window1 = vim.api.nvim_open_win(buffer1, true, {
     style="minimal", relative='editor',
-    row=3, col=50, width=100, height=30, border='single'
+    row=3, col=50, width=200, height=50, border='single'
   })
   
  
