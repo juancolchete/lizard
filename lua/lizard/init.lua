be7fc218ede1d8890b75f4d4fec0e3a952432c0d
juancolchete <juanudkp@gmail.com>
2023-06-18 00:43:18 -0300

    fix: string not apply on set buffer

--- lua/lizard/init.lua
+++ lua/lizard/init.lua
@@ -17,33 +17,15 @@ ___.....---"""        .       ""--..____
           |/\/\|    |/\/\|
 ]]
 
-function str_split (inputstr, sep)
-        if sep == nil then
-                sep = "%s"
-        end
-        local t={}
-        local i = 0
-        for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
-          print(str)
-          t[i] = str;
-          i = i + 1
-        end
-        return t
-end
-
 function Lizard.open(...)
   local buffer1 = vim.api.nvim_create_buf(false, true)
   for str in string.gmatch(drawRaw, "([^\n]+)") do
    vim.api.nvim_buf_set_lines(buffer1, 0, -1, true, {str})
   end
-  --for line in draw do
-    --print(line)
-    --vim.api.nvim_buf_set_lines(buffer1, 0, -1, true, {line})
-  --end
-  --vim.api.nvim_open_win(buffer1, true, {
-  --  style="minimal", relative='editor',
-  --  row=3, col=50, width=200, height=50, border='single'
-  --})
+  vim.api.nvim_open_win(buffer1, true, {
+    style="minimal", relative='editor',
+    row=3, col=50, width=200, height=50, border='single'
+  })
 end
 
 
